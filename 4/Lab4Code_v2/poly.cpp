3da8c59bffef05c996fa2c1d597be08edd5bb12c
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2023-11-14 21:55:32 -0500

    Initial skeleton

---
+++ 4/Lab4Code_v2/poly.cpp
@@ -0,0 +1,70 @@
+#include "poly.h"
+
+polynomial::polynomial()
+{
+
+}
+
+template <typename Iter>
+polynomial::polynomial(Iter begin, Iter end)
+{
+
+}
+
+void polynomial::print() const
+{
+
+}
+
+polynomial& polynomial::operator=(const polynomial &other)
+{
+
+}
+
+size_t polynomial::find_degree_of()
+{
+
+}
+
+std::vector<std::pair<power, coeff>> polynomial::canonical_form() const
+{
+
+}
+
+polynomial polynomial::operator+(int val) const 
+{
+
+}
+
+polynomial polynomial::operator+(const polynomial &other) const 
+{
+
+}
+
+polynomial operator+(int val, const polynomial &other)
+{
+
+}
+
+polynomial polynomial::operator*(int val) const 
+{
+
+}
+
+polynomial polynomial::operator*(const polynomial &other) const 
+{
+
+}
+
+polynomial operator*(int val, const polynomial &other)
+{
+
+}
+
+polynomial polynomial::operator%(const polynomial &other) const 
+{
+
+}
+
+
+
